faa7eb82ce5bae91e35d217c2c89cea9c4fb47fe
Vidya Sagar <38996713+vsagar7@users.noreply.github.com>
2018-12-06 04:28:18 +0530

    Add files via upload
    Two java codes and the pdf which contains screen shots are attached

---
+++ MyKafkaProducer.java
@@ -0,0 +1,42 @@
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MyKafkaProducer {
+  public static void main(String[] args) throws IOException{
+    if (args.length != 2) {
+      System.out.println("Please provide appropriate command line arguments");
+      System.exit(-1);
+    }
+
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+    ProducerRecord<String, String> producerRecord = null;
+
+    String fileName = args[0];
+    String delimiter = args[1];
+    
+    try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+        for(String line; (line = br.readLine()) != null; ) {
+            String[] tempArray = line.split(delimiter);
+            String topic = tempArray[0];
+            String key = tempArray[1];
+            String value = tempArray[2];
+
+            producerRecord = new ProducerRecord<String, String>(topic, key, value);
+        	producer.send(producerRecord);
+        	System.out.printf("Record sent to topic:%s. Key:%s, Value:%s\n", topic, key, value);
+        }
+    }
+    producer.close();
+  } 
+}
